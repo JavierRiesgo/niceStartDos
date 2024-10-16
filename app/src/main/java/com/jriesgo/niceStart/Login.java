e61127f21933af490ab00a862fe4d6017c3448be
JavierRiesgo <fjriesgosantos@gmail.com>
2024-10-16 14:01:44 +0200

    Initial commit

---
+++ app/src/main/java/com/jriesgo/niceStart/Login.java
@@ -0,0 +1,51 @@
+package com.jriesgo.niceStart;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
+import java.time.Instant;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);//Primero establece el layout
+
+
+        //creamos el identificador del ImageView
+        ImageView mGirl = findViewById(R.id.girl);
+
+        //cargar las dependencias de Glide
+        Glide.with(this)
+                .load("https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80")
+                .transition(DrawableTransitionOptions.withCrossFade(2000))
+                .centerCrop()
+                .into(mGirl);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void openSingup(View v){
+        Intent intent = new Intent(Login.this, SingUp.class);
+        startActivity(intent);
+    }
+}
+
+
+
---
+++ app/src/main/java/com/jriesgo/niceStart/SingUp.java
@@ -0,0 +1,32 @@
+package com.jriesgo.niceStart;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SingUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sing_up);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void openSingup(View v){
+        Intent intent = new Intent(SingUp.this, SingUp.class);
+        startActivity(intent);
+    }
+}
